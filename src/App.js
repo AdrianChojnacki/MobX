705a1e7bb7b9750641b220822f8aec8ce4aeb68d
Michał Zieliński (dev) <michal.zielinski@crafton.pl>
2022-02-10 19:41:35 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>LALALA</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A repellendus animi magnam! Pariatur hic fugiat quo ipsum quas doloremque voluptatibus eos voluptas officia beatae officiis possimus doloribus, labore voluptatem laborum.
+      </p>
     </div>
-  );
+  )
 }
 
 export default App;
